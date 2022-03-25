baa68abf1b3447bc14c3cc0f1adbbcf9a8b595ed
alekjs <random0463@gmail.com>
2022-03-26 08:38:35 +1100

    added env file

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
+require('dotenv').config();
 const mysql = require('mysql');
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
 const port = 3000;
+const dir = {root: __dirname};
 
 app.get('/', (req, res) => {
-  res.sendFile('./public/main.html', {root: __dirname})
+  res.sendFile('./public/main.html', dir);
 })
+
 app.get('/database', (res, req) => {
-const config = mysql.createConnection({
-user: 'root',
-host: 'localhost',
-password: 'Password321!',
-database: 'customerdb'
-});
-config.connect(function(err) {
+ const config = mysql.createConnection({
+  user: process.env.USER,
+  host: process.env.HOST,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+  });
+addata = `select * from customerdb.customers;`
+  config.query(addata, function (err, result) {
     if (err) throw err;
-    config.query("select * from customerdb.customers;", function (err, result) {
-      if (err) throw err;
-      req.send(result);
-    });
+    req.send(result);
   });
 })
 
